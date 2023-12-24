a1085db721ad1b6f5580ac07c4520631070338df
Rajiv Aryal <58936200+Aryal-rajiv@users.noreply.github.com>
2023-12-24 15:38:30 +0545

    Adding the calculator initial

---
+++ src/components/Calculator.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default Navigation();
+function Navigation(){
+    return(
+        <h5> I am writing logic to calculae love percenage </h5>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Navbar() {
+    return(
+              <h1>Hello Its me Navigation bar</h1>
+    )
+}
+export default Navbar;
